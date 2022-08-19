d5555e857d895c7534c0ff2ff6415165fb466939
Aakash Rathore <aakash.rathore321@gmail.com>
2022-08-19 15:17:28 +0530

    Created mock backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: "This is feedback item 1", rating: 10 },
-    { id: 2, text: "This is feedback item 2", rating: 9 },
-    { id: 3, text: "This is feedback item 3", rating: 8 },
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+    setFeedback(data)
+  };
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
 
   //To delete feedback
   const deleteFeedback = (id) => {
